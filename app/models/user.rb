1281293df1c0a8ab69e787be95810d71ba4c7cf3
Daniel Leining <daniel@the-beach.co>
2013-03-22 13:45:22 -0400

    username and time improvmentsx

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   #      :recoverable, :rememberable, :trackable, :validatable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
   has_many :posts
   has_many :comments
