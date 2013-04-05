b1505d7c256deb09aea05e513ccd856e73b7f9b5
Daniel Leining <daniel@the-beach.co>
2013-04-05 00:18:37 -0400

    Avatar helper and implement gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,30 @@ module ApplicationHelper
 		f = truncate(f, :length => Random.rand(200..300))
 		return f
 	end
+
+	def user_avatar_url(user, size)
+		if user.avatar.present?
+			if (size == 'thumb')
+				user.avatar.url(:thumb)
+			elsif (size == 'medium')
+				user.avatar.url(:medium)
+			elsif (size == 'small')
+				user.avatar.url(:small)
+			else
+				user.avatar.url
+			end
+		else
+			if (size == 'thumb')
+				size2 = 64
+			elsif (size == 'small')
+				size2 = 100
+			elsif (size == 'medium')
+				size2 = 300
+			else
+				size2 = 80
+			end
+			gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+			"http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size2}&d=identicon"
+		end
+	end
 end
