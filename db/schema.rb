eb01f39bf5aa8fc36d2070c76866485780780e8d
Daniel Leining <daniel@the-beach.co>
2013-04-08 01:19:19 -0400

    move to amazon rds

--- db/schema.rb
+++ db/schema.rb
@@ -53,18 +53,12 @@ ActiveRecord::Schema.define(:version => 20130405213439) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
-
-  create_table "user_avatars", :force => true do |t|
-    t.integer "user_id"
-    t.string  "style"
-    t.binary  "file_contents"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
